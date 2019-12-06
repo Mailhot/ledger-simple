7a0a24b12ea7d8071f768f314d203ff7a89247ed
Mailhot <36643298+Mailhot@users.noreply.github.com>
2019-12-06 16:36:39 -0500

    Create object.py
    Initial draft

---
+++ objects.py
@@ -0,0 +1,35 @@
+
+from mongoengine import *
+from datetime import datetime
+
+
+default_currency = "CAD"
+
+
+
+class Account(Document):
+	"""docstring for Document."""
+	parent = ReferenceField("self", default=None)
+	child = ReferenceField("self", default=None)
+	description = StringField(max_length=200, required=True)
+
+
+class Transaction(Document):
+	"""docstring for ClassName"""
+	account_to = ReferenceField("Account", required=True)
+	account_from = ReferenceField("Account", required=True)
+	amount = FloatField(default=1)
+	currency = StringField(max_length=3, default="CAD")
+	currency_rate = FloatField(default=1)
+	date = DateTimeField(default=datetime.now())
+
+def add_transaction(account_to, account_from, amount, currency=default_currency, date=datetime.now()):
+	# keep on
+
+def get_exchange_rate():
+	# Keep on
+
+
+	
+
+		
