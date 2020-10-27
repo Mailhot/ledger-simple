a14e2e8c63450eec41839b5de27c6d473121c113
Mailhot <36643298+Mailhot@users.noreply.github.com>
2020-10-27 14:51:48 -0400

    added function.py

---
+++ function.py
@@ -0,0 +1,16 @@
+import objects
+import unittest
+
+
+
+
+
+class StatementFunction():
+	"""docstring for Statement"""
+	def __init__(self, arg):
+		super(Statement, self).__init__()
+		self.arg = arg
+
+	def import_statemet_line(file, ):
+
+		
